a9cf46fe860d0839a214bd19b0b5ee14a82f423c
Ben <ben.mcevoy@gmail.com>
2019-03-31 19:02:28 +1100

    update CV
    it's been awhile

---
+++ PdfGen/PdfGen/Program.cs
@@ -0,0 +1,28 @@
+﻿using TuesPechkin;
+using System.Net;
+using System.IO;
+using Pechkin;
+
+namespace PdfGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wc = new WebClient();
+            var raw = wc.DownloadString("http://benmcevoy.com.au/resume.htm");
+
+            byte[] pdf = new SimplePechkin(
+                new Pechkin.GlobalConfig()).Convert(
+                new Pechkin.ObjectConfig()
+                    .SetLoadImages(true)
+                    .SetPrintBackground(true)
+                    .SetAllowLocalContent(true)
+                    .SetScreenMediaType(true)
+                    .SetCreateExternalLinks(true), raw);
+
+
+            File.WriteAllBytes("resume.pdf", pdf);
+        }
+    }
+}
