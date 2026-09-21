898914d39023d9c63dec1f1c01433c95cc8a5cad
ben <ben.mcevoy@gmail.com>
2026-09-21 17:11:55 +1000

    stuff

--- projects/dimensions/scripts/dimension.js
+++ projects/dimensions/scripts/dimension.js
@@ -8,13 +8,19 @@ class Dimension extends HTMLElement {
     container = document.createElement("div");
     rangeInput = document.createElement("input");
     rangeLabel = document.createElement("label");
+    minRangeLabel = document.createElement("small");
+    maxRangeLabel = document.createElement("small");
     constructor() {
         super();
         this.rangeInput.type = "range";
         this.rangeInput.min = "0";
         this.rangeInput.max = "1";
         this.container.className = "x-dimension";
+        this.container.title = this.title;
+        this.maxRangeLabel.style.float = "right";
         this.container.appendChild(this.rangeLabel);
+        this.container.appendChild(this.minRangeLabel);
+        this.container.appendChild(this.maxRangeLabel);
         this.container.appendChild(this.rangeInput);
     }
     get label() { return this.getAttribute("label") ?? ""; }
@@ -24,17 +30,23 @@ class Dimension extends HTMLElement {
     get valueAsNumber() { return this.rangeInput.valueAsNumber; }
     get quantization() { return Quantization[(this.getAttribute("quantization") ?? Quantization.Continuous)]; }
     set quantization(value) { this.setAttribute("quantization", value); }
+    get minLabel() { return this.getAttribute("min-label") ?? ""; }
+    set minLabel(value) { this.setAttribute("min-label", value); }
+    get maxLabel() { return this.getAttribute("max-label") ?? ""; }
+    set maxLabel(value) { this.setAttribute("max-label", value); }
     connectedCallback() {
         if (this.querySelector("div.x-dimension"))
             return;
         const step = (this.quantization == Quantization.Boolean)
             ? "1" : "any";
-        const id = `{this.id}-input`;
+        const id = `${this.id}-input`;
         this.rangeLabel.htmlFor = id;
         this.rangeLabel.textContent = `${this.label}:`;
+        this.minRangeLabel.textContent = `${this.minLabel}`;
+        this.maxRangeLabel.textContent = `${this.maxLabel}`;
         this.rangeInput.id = id;
         this.rangeInput.step = step;
-        this.rangeInput.value = this.defaultValue;
+        this.rangeInput.value = this.getAttribute("value") ?? this.defaultValue;
         this.append(this.container);
     }
 }
